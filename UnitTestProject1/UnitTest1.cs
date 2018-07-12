3e2b91b9a81b15696b93454bf73f273cf81a796e
Petr Závodný <vessago@seznam.cz>
2018-07-12 08:59:42 +0200

    before push to gitlab

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using Controls;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1 {
+    [TestClass]
+    public class UnitTest1 {
+        [TestMethod]
+        public void TestMethod1() {
+            StringAssert.StartsWith("ABC012",Ctrls.Transform("XYZ789", true));
+        }
+    }
+}
